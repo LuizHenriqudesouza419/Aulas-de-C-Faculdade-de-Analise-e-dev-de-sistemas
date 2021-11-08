6c31dd230e93dee2c715c2ff39de1ff15dec3ba9
Luiz Henrique de Souza <51526688+LuizHenriqudesouza419@users.noreply.github.com>
2021-11-08 19:54:37 -0300

    update

---
+++ Aula2-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main (int argc, char** argv){
+    int contador;
+
+    printf("\nDigite um valor: ");
+    scanf("%d", &contador);
+
+    for(contador; contador >=1; contador-- )
+    {
+        printf("%d ", contador);
+    }
+    return 0;
+}
