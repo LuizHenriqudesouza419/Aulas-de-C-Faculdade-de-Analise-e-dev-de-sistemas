f8edb665d09712a32772436f3f39d96fda193e7e
Luiz Henrique de Souza <51526688+LuizHenriqudesouza419@users.noreply.github.com>
2021-11-08 19:13:31 -0300

    update

---
+++ Aula-02-1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    float salario, inss, ir, sal_liquido;
+
+    printf("Digite seu salario bruto: ");
+    scanf("%f", &salario);
+
+    if (salario <= 1693.72)
+    {
+        inss =salario * 0.08;
+    }
+    else
+    if(salario >= 1693.73 && salario <= 2822.90)
+    {
+        inss = salario * 0.09;
+    }
+
+    sal_liquido = (salario - inss ) -ir;
+
+    printf("\n Desconto inss %f\n", inss);
+
+    printf("\n Salario liquido %2.f\n", sal_liquido);
+  
+    return 0;
+}
