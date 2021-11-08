bebf40b33d0f6d4a5523e158b29f7aa1c6c57efa
Luiz Henrique de Souza <51526688+LuizHenriqudesouza419@users.noreply.github.com>
2021-11-08 19:38:41 -0300

    update

---
+++ Aula02-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main (int argc, char** argv){
+    int i; 
+    do{
+        printf("\n Digite um numero do sabor\n");
+
+        printf("\t (1) ...Flocos \n)");
+        printf("\t (2) ...Morango \n");
+        printf("\t (3) ...Chocolate \n");
+
+        scanf("%d", &i);
+
+    }while ((i<1) || (i>3));
+
+    switch(i){
+        case 1:
+            printf("\t\t Vc escolhru flocos\n");
+            break;
+    
+        case 2:
+            printf("\t\t Vc escolhru Morango\n");
+            break;
+        
+         case 3:
+            printf("\t\t Vc escolhru chocolate\n");
+            break;
+    }
+    
+    return 0;
+}
